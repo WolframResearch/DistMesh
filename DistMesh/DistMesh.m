8b46e8a876099efd95fbb78f3f2787e3bcfdf142
Oliver Ruebenkoenig <ruebenko@wolfram.com>
2015-04-01 14:27:46 +0200

    Initial

---
+++ DistMesh/DistMesh.m
@@ -0,0 +1,14 @@
+(* Wolfram Language Package *)
+
+BeginPackage["DistMesh`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2015.04.01*)
+
+Paclet[
+  Name -> "DistMesh",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
